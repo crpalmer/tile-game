3bbbcd7aaca4af2de6799e0781c3cd115b8ea22f
Christopher R. Palmer <crpalmer@gmail.com>
2023-01-20 15:04:42 -0500

    Add fade in/out on scene changes

--- GameEngine/GameState.gd
+++ GameEngine/GameState.gd
@@ -3,7 +3,6 @@ extends Node
 var object_state = {}
 var player
 var paused = false
-var place_player_at
 
 func get(object_name:String, element_name = null):
 	if not object_state.has(object_name):
@@ -21,15 +20,24 @@ func resume():
 	paused = false
 
 func enter_scene(scene:String, entry_point:String):
+	if not player: player = load("res://GameEngine/Actors/Player.tscn").instance()
+	else:
+		var fade = player.get_node("Camera2D/HUD/Fade")
+		var anim:AnimationPlayer = fade.get_node("AnimationPlayer")
+		anim.play("Fade")
+		print("anim1 start")
+		yield(anim, "animation_finished")
+		print("anim1 finish")
+	
 	get_tree().current_scene.remove_child(player)
 	get_tree().change_scene(scene)
-	place_player_at = entry_point
 	
-func _process(_delta):
-	if place_player_at:
-		var scene = get_tree().current_scene
-		var entry_node = scene.get_node(place_player_at)
-		player.position = entry_node.position
-		scene.add_child(player)
-		player.enter_current_scene()
-		place_player_at = null
+	yield(get_tree(), "idle_frame")
+	
+	var anim:AnimationPlayer = player.get_node("Camera2D/HUD/Fade/AnimationPlayer")
+	var scene_node = get_tree().current_scene
+	var entry_node = scene_node.get_node(entry_point)
+	player.position = entry_node.position
+	scene_node.add_child(player)
+	player.enter_current_scene()
+	anim.play_backwards("Fade")
--- GameEngine/Main.gd
+++ GameEngine/Main.gd
@@ -4,6 +4,4 @@ export var scene:String
 export var entry_point:String
 
 func _ready():
-	GameState.player = $Player
 	GameState.enter_scene(scene, entry_point)
-	var level = load(filename)
